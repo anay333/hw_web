21651afcd7876f7e864c3bd13b17739b2e674f7a
anay333 <anay_1991@bk.ru>
2022-02-06 22:16:08 +0500

    установка с webDriverManager

---
+++ src/test/java/ru.netology.order/orderTest.java
@@ -0,0 +1,53 @@
+package ru.netology.order;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+class orderTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void shouldTestOrderPage() {
+        driver.get("http://localhost:9999");
+        // driver.findElement().sendKeys("Анна Горчилина");
+       // driver.findElement().sendKeys("89273031991");
+       List<WebElement> textFields = driver.findElements(By.className("input__control"));
+       textFields.get(0).sendKeys("Анна Горчилина");
+       textFields.get(1).sendKeys("+79273031991");
+       driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String actualMessage = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expectedMessage = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expectedMessage,actualMessage,"Фактическое не равно актуальному");
+    }
+
+}
