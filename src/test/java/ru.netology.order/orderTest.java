9103606770c35d61179a64c2f3eda80dbfcbc85b
anay333 <anay_1991@bk.ru>
2022-02-06 22:35:22 +0500

    добавлено разрешение gradlew

--- src/test/java/ru.netology.order/orderTest.java
+++ src/test/java/ru.netology.order/orderTest.java
@@ -1,4 +1,5 @@
 package ru.netology.order;
+
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -39,15 +40,15 @@ class orderTest {
     void shouldTestOrderPage() {
         driver.get("http://localhost:9999");
         // driver.findElement().sendKeys("Анна Горчилина");
-       // driver.findElement().sendKeys("89273031991");
-       List<WebElement> textFields = driver.findElements(By.className("input__control"));
-       textFields.get(0).sendKeys("Анна Горчилина");
-       textFields.get(1).sendKeys("+79273031991");
-       driver.findElement(By.className("checkbox__box")).click();
+        // driver.findElement().sendKeys("89273031991");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Анна Горчилина");
+        textFields.get(1).sendKeys("+79273031991");
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
         String actualMessage = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
         String expectedMessage = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        Assertions.assertEquals(expectedMessage,actualMessage,"Фактическое не равно актуальному");
+        Assertions.assertEquals(expectedMessage, actualMessage, "Фактическое не равно актуальному");
     }
 
 }
